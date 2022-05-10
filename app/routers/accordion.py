0be228e0c85dd199bc1d4e259ae3831af21c4d14
Nicholas Payne <nic.payne@protonmail.com>
2022-05-10 06:27:05 -0500

    remove unused import

--- app/routers/accordion.py
+++ app/routers/accordion.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, FastAPI, Form, Request
+from fastapi import APIRouter, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
